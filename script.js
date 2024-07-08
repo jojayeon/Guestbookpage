5fff8066a01c5c989aef46290125882453f58e64
jojayeon <jojayeon6152@gmail.com>
2024-07-08 16:33:43 +0900

    script.js에 버튼으로 숨기기 기능, 아래 포스트 방식 이해필요

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
-// html에 적용한 html
+// html에 적용한 js
+document.addEventListener('DOMContentLoaded', () => {
+  const toggleButton = document.getElementById('toggleButton');
+  const formContainer = document.getElementById('formContainer');
+  const form = document.getElementById('formContainer');
+
+  toggleButton.addEventListener('click', () => {
+      if (formContainer.classList.contains('hidden')) {
+          formContainer.classList.remove('hidden');
+      } else {
+          formContainer.classList.add('hidden');
+      }
+  });
+
+  form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      const title = document.getElementById('titleInput').value;
+      const content = document.getElementById('contentInput').value;
+
+      const data = { title, content };
+
+      fetch('http://localhost:3000/post-submit', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+      })
+      .then(response => response.json())
+      .then(result => {
+          console.log('Success:', result);
+      })
+      .catch(error => {
+          console.error('Error:', error);
+      });
+  });
+})
